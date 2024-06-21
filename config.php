4b1fc605582def917483fb4d550fab80c19fe42b
Subhasisa Sahoo <subhasisasahoo098@gmail.com>
2024-06-21 16:55:00 +0530

    Delete config.php file contains the email & pwd

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'smtp_host' => 'smtp.gmail.com',
-    'smtp_username' => 'craftshop.live@gmail.com',
-    'smtp_password' => 'bwui jock bnsh oeqn',
-    'smtp_port' => 465,
-    'smtp_secure' => PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS,
-];
